a364dbf973307b194397b8f4098a00e5637b32ff
goncaloalexandrefgomes <goncalo.gomes1412003@gmail.com>
2020-11-14 12:04:37 +0000

    grande avanco 3

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/piano')
+def piano():
+    return render_template('piano.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
