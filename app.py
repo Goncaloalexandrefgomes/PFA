9d7859c638367ecd250f9f85f32c8f8a2ef10484
goncaloalexandrefgomes <goncalo.gomes1412003@gmail.com>
2020-11-15 12:31:11 +0000

    login

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def index():
 def piano():
     return render_template('piano.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
